46c4b5be68dab825da47eb4d0975a6c4c2e36fe5
bella13 <isabellad.13@icloud.com>
2018-07-12 19:25:14 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,30 @@
 # Make your shoe class here!
+
 class Shoe
   
-  
   def intitialize(brand)
+    @brand = brand
  end 
 
-def brand
+ def brand
+   @brand
 end
+
+def color=(color)
+  @color = color 
+  end 
+
+def color
+  @color
+end 
+
+
 end
+
+
+
+
+
+
+
+
